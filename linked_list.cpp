bad5aaf386304b814bd108c4ead8f3784f568caf
Md-Tanvir2034 <studynote2034@gmail.com>
2025-09-06 00:45:37 +0600

    6

--- linked_list.cpp
+++ linked_list.cpp
@@ -29,18 +29,29 @@ class List {
     void push_front(int val){
         Node* newNode = new Node(val);
         if(head == NULL){
-            if(head == NULL){
-                head = tail = newNode;
-                return;
-            } else {
-                newNode -> next = head ;
-                head = newNode ;
-            }
+            head = tail = newNode;
+            return;
+        } else {
+            newNode -> next = head ;
+            head = newNode ;
         }
 
     }
 
-}
+    void printLL(){
+        Node* temp = head ;
+
+        while(temp != NULL){
+            cout << temp -> data  << " " ;
+            temp = temp -> next ;
+        }
+
+        cout << endl ;
+        
+        
+    }
+
+};
 
 
 
@@ -49,9 +60,11 @@ int main(){
 
     List myList;
 
-        myList.push_front(10);
+        myList.push_front(10); 
         myList.push_front(20);
         myList.push_front(30);
+
+        myList.printLL();
 
     return 0 ;
 }
